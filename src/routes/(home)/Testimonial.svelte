3338129b3d0aba90f690efedcc6013d3c1169384
Clembs <clembs@clembs.com>
2025-06-21 22:29:29 +0200

    refactor: move testimonial to its own component + various design fixes

---
+++ src/routes/(home)/Testimonial.svelte
@@ -0,0 +1,167 @@
+<script lang="ts">
+	export let quote: string;
+	export let profile: {
+		name: string;
+		position: string;
+		avatarUrl: string;
+		brandName: string;
+		brandLogoUrl: string;
+	};
+</script>
+
+<li class="testimonial-wrapper">
+	<div class="testimonial-border">
+		<div class="testimonial">
+			<div class="quote">
+				{@html quote}
+			</div>
+			<div class="author">
+				<div class="profile">
+					<img
+						draggable="false"
+						src={profile.avatarUrl}
+						alt={profile.name}
+						height="40"
+						width="40"
+						class="avatar"
+					/>
+					<div class="text">
+						<div class="name">{profile.name}</div>
+						<p class="position">{profile.position}</p>
+					</div>
+				</div>
+				<img
+					draggable="false"
+					src={profile.brandLogoUrl}
+					alt="{profile.brandName}'s logo"
+					class="logo"
+				/>
+			</div>
+		</div>
+	</div>
+
+	<!-- Quote icon -->
+	<svg
+		class="quote-icon"
+		width="35"
+		height="28"
+		viewBox="0 0 35 28"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<path
+			d="M5.34242 17.1328C6.16667 15.9141 7.50606 14.8984 9.77273 14.8984C13.1727 14.8984 15.8515 17.5391 15.8515 21.0938C15.8515 25.3594 12.5545 28 8.53636 28C4.62121 28 0.5 25.0547 0.5 18.5547C0.5 10.6328 5.65152 3.45313 12.7606 0L13.3788 3.11719C7.19697 6.67188 4.10606 10.0234 4.10606 14.2891C4.10606 15.7109 4.62121 16.7266 5.34242 17.1328ZM23.9909 17.1328C24.8152 15.9141 26.1545 14.8984 28.4212 14.8984C31.8212 14.8984 34.5 17.5391 34.5 21.0938C34.5 25.3594 31.203 28 27.1848 28C23.2697 28 19.1485 25.0547 19.1485 18.5547C19.1485 10.6328 24.3 3.45313 31.4091 0L32.0273 3.11719C25.8455 6.67188 22.7545 10.0234 22.7545 14.2891C22.7545 15.7109 23.2697 16.7266 23.9909 17.1328Z"
+			fill="url(#paint0_linear_1624_3344)"
+		/>
+		<defs>
+			<linearGradient
+				id="paint0_linear_1624_3344"
+				x1="17.5"
+				y1="2"
+				x2="17.5"
+				y2="28"
+				gradientUnits="userSpaceOnUse"
+			>
+				<stop stop-color="white" />
+				<stop offset="1" stop-color="white" stop-opacity="0.2" />
+			</linearGradient>
+		</defs>
+	</svg>
+</li>
+
+<style lang="scss">
+	.testimonial-wrapper {
+		position: relative;
+		height: fit-content;
+	}
+
+	.quote-icon {
+		position: absolute;
+		top: -28px;
+		left: 50%;
+		transform: translate(-50%, 50%);
+	}
+
+	.testimonial-border {
+		--space-from-center: 35px;
+		--border-size: 1px;
+
+		padding: var(--border-size);
+		background-image: linear-gradient(
+			180deg,
+			var(--color-foreground-lowest),
+			var(--color-foreground-lowerest)
+		);
+		border-radius: calc(var(--space-lg) + var(--border-size));
+		height: fit-content;
+		clip-path: polygon(
+			0 0,
+			0 100%,
+			100% 100%,
+			100% 0,
+			calc(50% + var(--space-from-center)) 0,
+			calc(50% + var(--space-from-center)) var(--border-size),
+			calc(50% - var(--space-from-center)) var(--border-size),
+			calc(50% - var(--space-from-center)) 0
+		);
+	}
+
+	.testimonial {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: var(--space-lg);
+		padding: var(--space-xl);
+		border-radius: var(--space-lg);
+		background-color: var(--color-background);
+
+		.quote {
+			font-size: var(--font-size-md);
+			color: var(--color-foreground-low);
+
+			:global(.highlight) {
+				color: var(--color-foreground-full);
+			}
+		}
+
+		.author {
+			display: flex;
+			justify-content: space-between;
+			gap: var(--space-md);
+			align-items: center;
+			width: 100%;
+			padding-left: var(--space-md);
+			border-left: 2px solid var(--color-foreground-low);
+
+			.profile {
+				display: grid;
+				grid-template-columns: auto 1fr;
+				gap: var(--space-md);
+				align-items: center;
+
+				.avatar {
+					border-radius: 50%;
+					background-color: var(--color-foreground-low);
+					font-size: 0px;
+				}
+
+				.name {
+					font-weight: 500;
+					color: var(--color-foreground-full);
+				}
+
+				.position {
+					font-size: var(--font-size-sm);
+					text-wrap: balance;
+					opacity: 0.75;
+				}
+			}
+
+			.logo {
+				width: auto;
+				height: 30px;
+				max-width: 45px;
+			}
+		}
+	}
+</style>
--- src/routes/(home)/Testimonials.svelte
+++ src/routes/(home)/Testimonials.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import Quote from '$lib/svg/Quote.svelte';
 	import { onMount } from 'svelte';
+	import Testimonial from './Testimonial.svelte';
 
 	let sectionEl: HTMLElement;
 	let sectionAnimationPercent = 0;
@@ -10,7 +10,7 @@
 		const sectionRect = sectionEl.getBoundingClientRect();
 
 		sectionAnimationPercent = Math.max(
-			(window.innerHeight - (sectionRect.top - sectionRect.height / 2)) / window.innerHeight,
+			(window.innerHeight - (sectionRect.top - sectionRect.height / 1.5)) / window.innerHeight,
 			0
 		);
 	}
@@ -27,101 +27,39 @@
   transform: translateY(calc(-50px * {sectionAnimationPercent}));"
 >
 	<ul id="testimonials">
-		<li class="testimonial-wrapper">
-			<div class="testimonial">
-				<div class="quote-symbol">
-					<Quote />
-				</div>
-
-				<div class="quote">
-					<span class="highlight">Incredible output, organised, diligent, proactive...</span>
-					I'm privileged to have him on our Team at Catalyst Chemistry.
-				</div>
-				<div class="author">
-					<div class="profile">
-						<img
-							src="/testimonials/avatars/jeff-tse.png"
-							alt="Jeff Tse"
-							height="40"
-							width="40"
-							class="avatar"
-						/>
-						<div class="text">
-							<div class="name">Jeff Tse</div>
-							<p class="position">Founder @ Catalyst Chemistry</p>
-						</div>
-					</div>
-
-					<img
-						src="/testimonials/logos/catalyst-chemistry.svg"
-						alt="Catalyst Chemistry's logo"
-						class="logo"
-					/>
-				</div>
-			</div>
-		</li>
-		<li class="testimonial-wrapper">
-			<div class="testimonial">
-				<div class="quote-symbol">
-					<Quote />
-				</div>
-
-				<div class="quote">
-					James is great to work with. He's <span class="highlight">dedicated</span> to his craft...
-					and produces <span class="highlight">great quality UX/UI</span> output.
-				</div>
-				<div class="author">
-					<div class="profile">
-						<img
-							src="/testimonials/avatars/daniel-radovich.png"
-							alt="Daniel Radovich"
-							height="40"
-							width="40"
-							class="avatar"
-						/>
-						<div class="text">
-							<div class="name">Daniel Radovich</div>
-							<p class="position">Director, Lead Product Designer @ Raw Studio</p>
-						</div>
-					</div>
-
-					<img src="/testimonials/logos/raw.svg" alt="Raw Studio's logo" class="logo" />
-				</div>
-			</div>
-		</li>
-		<li class="testimonial-wrapper">
-			<div class="testimonial">
-				<div class="quote-symbol">
-					<Quote />
-				</div>
-
-				<div class="quote">
-					James is one of the few designers I know that makes you think
-					<span class="highlight">'why would I want it any other way?'.</span>
-				</div>
-				<div class="author">
-					<div class="profile">
-						<img
-							src="/testimonials/avatars/darren-candra.png"
-							alt="Darren Candra"
-							height="40"
-							width="40"
-							class="avatar"
-						/>
-						<div class="text">
-							<div class="name">Darren Candra</div>
-							<p class="position">Founder @ Subjective Labs</p>
-						</div>
-					</div>
-
-					<img
-						src="/testimonials/logos/subjective-labs.png"
-						alt="Subjective Labs's logo"
-						class="logo"
-					/>
-				</div>
-			</div>
-		</li>
+		<Testimonial
+			quote={`<span class="highlight">Incredible output, organised, diligent, proactive...</span>
+			I'm privileged to have him on our Team at Catalyst Chemistry.`}
+			profile={{
+				name: 'Jeff Tse',
+				avatarUrl: '/testimonials/avatars/jeff-tse.png',
+				position: 'Founder @ Catalyst Chemistry',
+				brandLogoUrl: '/testimonials/logos/catalyst-chemistry.svg',
+				brandName: 'Catalyst Chemistry'
+			}}
+		/>
+		<Testimonial
+			quote={`James is great to work with. He's <span class="highlight">dedicated</span> to his craft...
+			and produces <span class="highlight">great quality UX/UI</span> output.`}
+			profile={{
+				name: 'Daniel Radovich',
+				avatarUrl: '/testimonials/avatars/daniel-radovich.png',
+				position: 'Director, Lead Product Designer @ Raw Studio',
+				brandLogoUrl: '/testimonials/logos/raw.svg',
+				brandName: 'Raw Studio'
+			}}
+		/>
+		<Testimonial
+			quote={`James is one of the few designers I know that makes you think
+					<span class="highlight">'why would I want it any other way?'.</span>`}
+			profile={{
+				name: 'Darren Candra',
+				avatarUrl: '/testimonials/avatars/darren-candra.png',
+				position: 'Founder @ Subjective Labs',
+				brandLogoUrl: '/testimonials/logos/subjective-labs.png',
+				brandName: 'Subjective Labs'
+			}}
+		/>
 	</ul>
 </section>
 
@@ -129,6 +67,7 @@
 	#testimonials-section {
 		padding: var(--space-xxl) var(--space-xxl);
 		margin: 0 auto;
+		max-width: 1400px;
 
 		@media (max-width: 600px) {
 			padding: var(--space-md) var(--space-sm);
@@ -142,80 +81,5 @@
 		list-style: none;
 		padding: 0;
 		margin: 0 auto;
-	}
-
-	.testimonial-wrapper {
-		padding: 1px;
-		background-image: linear-gradient(
-			180deg,
-			var(--color-foreground-lowest),
-			var(--color-foreground-lowerest)
-		);
-		border-radius: calc(var(--space-lg) + 1px);
-		height: fit-content;
-	}
-
-	.testimonial {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		gap: var(--space-lg);
-		padding: var(--space-xl);
-		border-radius: var(--space-lg);
-		background-color: var(--color-background);
-
-		.quote-symbol {
-			background-color: var(--color-background);
-			margin-top: -45px;
-			padding: 0 1rem;
-		}
-
-		.quote {
-			font-size: var(--font-size-md);
-			color: var(--color-foreground-low);
-
-			.highlight {
-				color: var(--color-foreground-full);
-			}
-		}
-
-		.author {
-			display: flex;
-			justify-content: space-between;
-			gap: var(--space-md);
-			align-items: center;
-			width: 100%;
-			padding-left: var(--space-md);
-			border-left: 2px solid var(--color-foreground-low);
-
-			.profile {
-				display: grid;
-				grid-template-columns: auto 1fr;
-				gap: var(--space-md);
-				align-items: center;
-
-				.avatar {
-					border-radius: 50%;
-					background-color: var(--color-foreground-low);
-					font-size: 0px;
-				}
-
-				.name {
-					font-weight: 600;
-					color: var(--color-foreground-full);
-				}
-
-				.position {
-					font-size: var(--font-size-sm);
-					text-wrap: balance;
-				}
-			}
-
-			.logo {
-				width: auto;
-				height: 30px;
-				max-width: 45px;
-			}
-		}
 	}
 </style>
